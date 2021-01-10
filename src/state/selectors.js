932e21fc6bbeb492be4d89ee44fa286401d432ef
AmoDinho <u14284783@tuks.co.za>
2021-01-10 21:08:02 +0200

    have to rework how to set async setters

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -1,13 +1,13 @@
 import { selector } from "recoil";
 import axios from "axios";
+import { circuitState } from "./atoms";
 const circuitsQuery = selector({
   key: "circuits",
-  get: async () => {
+  set: async ({ set }) => {
     const circuitResponse = await axios.get(
       `http://ergast.com/api/f1/2021/circuits.json`
     );
-    console.log(circuitResponse);
-    return circuitResponse;
+    set(circuitState, circuitResponse.data.MRData.CircuitTable.Circuits);
   },
 });
 
