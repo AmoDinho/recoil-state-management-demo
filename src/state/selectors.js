e5a4e76346f07df86a63d48024764f4e080c7cf3
AmoDinho <u14284783@tuks.co.za>
2021-01-10 20:59:30 +0200

    working

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -4,9 +4,10 @@ const circuitsQuery = selector({
   key: "circuits",
   get: async () => {
     const circuitResponse = await axios.get(
-      `http://ergast.com/api/f1/2021/circuits`
+      `http://ergast.com/api/f1/2021/circuits.json`
     );
-    console.log("circuitResponse", circuitResponse);
+    console.log(circuitResponse);
+    return circuitResponse;
   },
 });
 
