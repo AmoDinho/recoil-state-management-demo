7d21531a52e39da158b8414d722b896cfe60a1a3
AmoDinho <u14284783@tuks.co.za>
2021-01-10 20:33:26 +0200

    Trying to display the drivers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { driverState } from "../state/atoms";
 import HeadingOne from "../components/HeadingOne";
 const Index = () => {
+  const [drivers, setDrivers] = useRecoilState(driverState);
   return (
     <>
       <HeadingOne className="text-center text-blue-500">
         This is the home page of the app!
       </HeadingOne>
+      {drivers.map((driver, index) => (
+        <div key={index}>{driver}</div>
+      ))}
     </>
   );
 };
