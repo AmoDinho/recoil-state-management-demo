903c131fc9ddc530f135df20557dc3900be51606
AmoDinho <u14284783@tuks.co.za>
2021-01-10 20:27:15 +0200

    adding the recoild root

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-
+import { RecoilRoot } from "recoil";
 //add the uri here
 const client = new ApolloClient({
   uri: "",
 });
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
