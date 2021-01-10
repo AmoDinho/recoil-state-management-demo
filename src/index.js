39919b0f34027dbd24c8b80aad5a8500e807327b
AmoDinho <u14284783@tuks.co.za>
2021-01-10 20:57:58 +0200

    call is working

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { circuitsQuery } from "./state/selectors";
 //add the uri here
 const client = new ApolloClient({
   uri: "",
 });
+
+console.log("circuitsQuery", circuitsQuery);
+
+const CircuitBootstrap = () => {
+  const circuits = useRecoilValue(circuitsQuery);
+  return <></>;
+};
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={<div>...loading</div>}>
       <RecoilRoot>
         <ApolloProvider client={client}>
+          <CircuitBootstrap />
           <App />
         </ApolloProvider>
       </RecoilRoot>
