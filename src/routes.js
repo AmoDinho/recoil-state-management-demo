ffd7fd100967407c14ef07484679771d5d163e15
AmoDinho <u14284783@tuks.co.za>
2021-01-13 22:13:57 +0200

    added driver component to root

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Home from "./pages/index";
+import Driver from "./pages/Driver";
 const Routes = ({ props }) => {
   return (
     <Router>
       <Home path="/" props={props} />
+      <Driver path="/driver/:driverID" props={props} />
     </Router>
   );
 };
