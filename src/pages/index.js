d71dc1878b18880bab4fad4717e8b115029ded4e
AmoDinho <u14284783@tuks.co.za>
2021-01-13 22:23:48 +0200

    added onclick event

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { driverState } from "../state/atoms";
 import HeadingOne from "../components/HeadingOne";
-const Index = () => {
+const Index = (props) => {
   const drivers = useRecoilValue(driverState);
+  console.log(props);
   return (
     <>
       <HeadingOne className="text-center text-blue-500">
         This is the home page of the app!
       </HeadingOne>
       {drivers.map((driver, index) => (
-        <div key={index}>{driver}</div>
+        <div
+          key={index}
+          onClick={() => props.navigate(`/driver/${driver.split(" ")[1]}`)}
+        >
+          {driver}
+        </div>
       ))}
     </>
   );
