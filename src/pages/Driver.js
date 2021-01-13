2beaf42e9bf344b388c3d95415c20dfc418d8be4
AmoDinho <u14284783@tuks.co.za>
2021-01-13 22:30:34 +0200

    donne withloading stuff

--- src/pages/Driver.js
+++ src/pages/Driver.js
@@ -2,12 +2,16 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { driverQuery } from "../state/selectors";
 const Driver = (props) => {
-  console.log("props", props);
   const driver = useRecoilValue(driverQuery(props.driverID));
+  console.log("props", driver);
 
   return (
     <>
-      <p>This is the page of [drivername]</p>
+      <p>This is the page of {driver.givenName}</p>
+      <p>He is from {driver.nationality}</p>
+      <p>
+        You can learn more about him <a href={driver.url}>HERE</a>{" "}
+      </p>
     </>
   );
 };
