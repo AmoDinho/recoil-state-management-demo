6b23ba5e9388df814706a304f3ef9e89796262cd
AmoDinho <u14284783@tuks.co.za>
2021-01-13 22:31:09 +0200

    everything is working

--- src/pages/Driver.js
+++ src/pages/Driver.js
@@ -3,12 +3,11 @@ import { useRecoilValue } from "recoil";
 import { driverQuery } from "../state/selectors";
 const Driver = (props) => {
   const driver = useRecoilValue(driverQuery(props.driverID));
-  console.log("props", driver);
 
   return (
     <>
       <p>This is the page of {driver.givenName}</p>
-      <p>He is from {driver.nationality}</p>
+      <p>He is {driver.nationality}</p>
       <p>
         You can learn more about him <a href={driver.url}>HERE</a>{" "}
       </p>
